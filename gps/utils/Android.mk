11008074f54777d6fc001721ca9ac6fd24bc00fd
MkfsSion <myychina28759@gmail.com>
2018-08-17 19:40:01 +0800

    ido: gps: Clean up makefile
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,6 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -61,7 +58,5 @@ LOCAL_COPY_HEADERS:= \
 LOCAL_MODULE := libgps.utils
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
